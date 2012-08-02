829b8bf130b36c3fbc9beaf6a0399bccd6adabaf
Vladimir Nachbaur <vladimir.nachbaur@gmail.com>
2012-08-02 13:22:40 +0200

    pyqt and py2exe tests

---
+++ foc-win/foc-win.py
@@ -0,0 +1,26 @@
+import os
+from PyQt4 import QtGui
+from PyQt4.QtCore import *
+
+class HelloPython(QtGui.QWidget):  
+    def __init__(self, parent=None):  
+        super(HelloPython, self).__init__(parent)  
+        helloLabel = QtGui.QLabel("Say Hello To PyQT!")  
+        helloLineEdit = QtGui.QLineEdit()  
+  
+        mainLayout = QtGui.QGridLayout()  
+        mainLayout.addWidget(helloLabel, 0, 0)  
+        mainLayout.addWidget(helloLineEdit, 0, 1)  
+  
+        self.setLayout(mainLayout)  
+        self.setWindowTitle("My Python App")  
+  
+if __name__ == '__main__':  
+    import sys  
+  
+    app = QtGui.QApplication(sys.argv)  
+  
+    helloPythonWidget = HelloPython()  
+    helloPythonWidget.show()
+  
+    sys.exit(app.exec_())
